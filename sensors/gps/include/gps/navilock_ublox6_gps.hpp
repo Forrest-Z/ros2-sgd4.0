c254362f92891ee7e44bc6b1eb01aef0c076eda1
ippmob103 <pascal.stahr@haw-hamburg.de>
2021-03-16 09:35:55 +0100

    Start of Navilock UBlox6 implementation

---
+++ sensors/gps/include/gps/navilock_ublox6_gps.hpp
@@ -0,0 +1,64 @@
+
+
+#ifndef NAVILOCK_UBLOX6_GPS_HPP_
+#define NAVILOCK_UBLOX6_GPS_HPP_
+
+#include <functional>
+#include <memory>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>
+#include <termios.h>
+#include <unistd.h>
+
+#include "rclcpp/rclcpp.hpp"
+//#include "sensor_msgs/msg/nav_sat_fix.hpp"
+//#include "geometry_msgs/msg/point_stamped.hpp"
+//#include "geometry_msgs/msg/point.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+namespace sensor_gps
+{
+
+class Navilock_UBlox6_GPS : public rclcpp::Node
+{
+public:
+
+  Navilock_UBlox6_GPS();
+  ~Navilock_UBlox6_GPS();
+
+protected:
+
+  // Implement the lifecycle interface -> TODO
+  //nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State & state) override;
+  //nav2_util::CallbackReturn on_activate(const rclcpp_lifecycle::State & state) override;
+  //nav2_util::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & state) override;
+  //nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & state) override;
+  //nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state) override;
+
+  void readLine();
+
+  // Publisher 
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  int serial_port_;
+  int const baud_rate_;
+  char const port_[80];
+  bool isPortOpen;
+  struct termios tty_;
+
+  void timer_callback();
+  int init();
+
+};
+
+}   // namespace sensor_gps
+
+#endif // NAVILOCK_UBLOX6_GPS_HPP_
---
+++ sensors/gps/src/navilock_ublox6_gps.cpp
@@ -0,0 +1,130 @@
+
+
+
+#include "gps/navilock_ublox6_gps.hpp"
+
+#include <functional>
+#include <memory>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>
+#include <termios.h>
+#include <unistd.h>
+
+#include "rclcpp/rclcpp.hpp"
+//#include "sensor_msgs/msg/nav_sat_fix.hpp"
+//#include "geometry_msgs/msg/point_stamped.hpp"
+//#include "geometry_msgs/msg/point.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+namespace sensor_gps
+{
+
+Navilock_UBlox6_GPS::Navilock_UBlox6_GPS():
+        baud_rate_(9600),port_("/dev/ttyACM0"),Node("navilock_ublox6_gps")
+{
+    init();
+    
+    if (isPortOpen_)
+    {
+      auto default_qos = rclcpp::QoS(rclcpp::SystemDefaultsQoS());
+    
+      publisher_ = this->create_publisher<std_msgs::msg::String>("gps", default_qos);
+      timer_ = this->create_wall_timer(1000ms, std::bind(&Navilock_UBlox6_GPS::readLine, this));
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Could not open port %s. Check connections and user rights.", port_);
+    }
+}
+
+Navilock_UBlox6_GPS::~Navilock_UBlox6_GPS()
+{
+  RCLCPP_INFO(this->get_logger(), "Destroying");
+}
+
+void
+Navilock_UBlox6_GPS::readLine() {
+  char read_buf [256];
+  std::memset(&read_buf, '\0', sizeof(read_buf));
+  int num_bytes = read(serial_port_, &read_buf, sizeof(read_buf));
+
+  if (num_bytes < 0) {
+    RCLCPP_ERROR(this->get_logger(), "Error reading %s", strerror(errno));
+    return;
+  }
+
+  int retries = 0;
+  while(num_bytes < 1 && retries < 5) {
+    num_bytes = read(serial_port_, &read_buf, sizeof(read_buf));
+
+    retries++;
+  }
+
+  if (retries >= 5)
+  {
+    RCLCPP_ERROR(this->get_logger(), "Could not receive valid input data.");
+    return;
+  }
+  
+  RCLCPP_DEBUG(this->get_logger(), "Read %i bytes. Received message: %s", num_bytes, read_buf);
+
+  auto msg_ = std_msgs::msg::String();
+  std::string s(read_buf);
+  msg_.data = s;
+  publisher_->publish(msg_);
+}
+
+int
+Navilock_UBlox6_GPS::init() {
+  serial_port_ = open(port_, O_RDWR);
+    isPortOpen_ = serial_port_ > 0 ? false : true;
+
+    if (tcgetattr(serial_port_, &tty_) != 0)
+    {
+        RCLCPP_ERROR(this->get_logger(), "Error %i from togetattr %s", errno, strerror(errno));
+        isPortOpen_ = false;
+        return 1;       // TODO: Error handling
+    }
+
+    tty_.c_cflag &= ~PARENB;     // clear parity bit
+    tty_.c_cflag &= ~CSTOPB;
+    tty_.c_cflag &= ~CSIZE;
+    tty_.c_cflag != CS8;
+    tty_.c_cflag &= ~CRTSCTS;
+    tty_.c_cflag != (CREAD | CLOCAL);
+
+    tty_.c_lflag &= ~ICANON;
+    tty_.c_lflag &= ~ECHO;
+    tty_.c_lflag &= ~ECHOE;
+    tty_.c_lflag &= ~ECHONL;
+    tty_.c_lflag &= ~ISIG;
+
+    tty_.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty_.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+
+    tty_.c_oflag &= ~OPOST;
+    tty_.c_oflag &= ~ONLCR;
+
+    tty_.c_cc[VTIME] = 10;
+    tty_.c_cc[VMIN] = 0;
+
+    cfsetispeed(&tty_, B9600);      // TODO: use user defined baud-rate
+    cfsetospeed(&tty_, B9600);
+
+    return 0;
+}
+
+}   // namespace sensor_gps
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<sensor_gps::Navilock_UBlox6_GPS>());
+  rclcpp::shutdown();
+  return 0;
+}
