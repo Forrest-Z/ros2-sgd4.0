698c665ed1479438a2c37334031a7043cfd3c9d0
Miguel Martinez <miguel.martinezgenis@haw-hamburg.de>
2022-06-23 15:52:58 +0200

    Determines if the robot can pass through in between obstacles.

--- sgd_safety/include/sgd_safety/depthCamera_obstacle_checker.hpp
+++ sgd_safety/include/sgd_safety/depthCamera_obstacle_checker.hpp
@@ -15,11 +15,10 @@ private:
     const int obstacle_sensitivity_local = 5;       // Minimum ocurrences per line to be considered an obstacle
     const int obstacle_sensitivity_global = 3;      // Minimum occurrences of obstacle_sensitivity_local to raise a detection flag.
     const int detection_distance = 130;             // How far (in pixels) will the objects be detected
-    const int detection_width = 20;                 // This value should be an odd number.
-    double robot_width_;         // Width of the robot in m including the wheels
-    double distance_min_;        // Minimal distance the robot has to keep with respect to obstacles
-    double distance_max_;        // At any distance smaller than this, the robot should start decreasing its speed.
-    const float width_tolerance = 0.1;      // A tolerance on the robot width in m for an extra safety factor.
+    const int detection_width = 360;                // This value should be large enough to guarantee the robot can pass through. It must also be an even number.
+    const float depthCamera_max_dist = 1.5;
+    const float depthCamera_max_dist = 1.5;
+    const float depthCamera_min_dist = 0.8;
     rs2::pipeline p;    // Create a Pipeline
     rs2::frameset frames;
     uint8_t init_val = 0;
